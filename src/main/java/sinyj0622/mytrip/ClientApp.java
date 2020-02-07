6d73ea0c46462832158512995a895b4aa5ef3e10
eunjeong <euuun22@naver.com>
2020-02-07 19:48:36 +0900

    메시지를 서버에 전송하고 응답받기

--- src/main/java/sinyj0622/mytrip/ClientApp.java
+++ src/main/java/sinyj0622/mytrip/ClientApp.java
@@ -1,9 +1,63 @@
 
 package sinyj0622.mytrip;
 
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientApp {
 
   public static void main(String[] args) {
     System.out.println("클라이언트 여행 관리 시스템입니다");
+
+    String serverAddr = null;
+    int port = 0;
+
+    Scanner keyScan = new Scanner(System.in);
+
+    try {
+      System.out.print("서버? ");
+      serverAddr = keyScan.nextLine();
+
+      System.out.print("포트? ");
+      port = Integer.parseInt(keyScan.nextLine());
+
+
+    } catch (Exception e) {
+      System.out.println("서버 주소 또는 포트 번호가 유효하지 않습니다!");
+      keyScan.close();
+      return;
+    }
+
+    try (Socket socket = new Socket(serverAddr, port);
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        Scanner in = new Scanner(socket.getInputStream())) {
+
+
+      System.out.println("서버와 연결되었음!");
+
+
+      System.out.print("서버에 보낼 메세지: ");
+      String sendMsg = keyScan.nextLine();
+
+
+      out.println(sendMsg);
+      System.out.println("서버에 메세지를 전송하였음!");
+
+
+      String message = in.nextLine();
+      System.out.println("서버로부터 메세지를 수신하였음!");
+
+
+      System.out.println("서버: " + message);
+
+
+      System.out.println("서버와 연결을 끊었음");
+    } catch (Exception e) {
+      System.out.println("예외 발생 :");
+      e.printStackTrace();
+    }
+
+    keyScan.close();
   }
 }
